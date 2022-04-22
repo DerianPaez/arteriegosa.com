3de42ec95302951bf8c04d74c44e06d926fc1d1d
James Mendoza <mendozajames02@hotmail.com>
2022-04-22 09:49:04 -0500

    Change map addrees

--- components/common/Map/Map.tsx
+++ components/common/Map/Map.tsx
@@ -19,7 +19,7 @@ const Map: React.FC<Props> = ({ ...props }) => {
     <MapStyled>
       <iframe
         title="map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3352.775028602812!2d-79.93826776934559!3d-2.0979668505320705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d0d416a07ba3f%3A0x343b7937a49a3d51!2sParque%20california%202%20bodegas!5e0!3m2!1ses!2sec!4v1639832796028!5m2!1ses!2sec"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1676.4240673840345!2d-79.91887101487922!3d-2.063581442795956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d13aff5646659%3A0xcc6434d36ebdc086!2sMetropolis%202%20etapa%20E!5e0!3m2!1ses!2sec!4v1650638871575!5m2!1ses!2sec"
         allowFullScreen
         loading="lazy"
         { ...props }

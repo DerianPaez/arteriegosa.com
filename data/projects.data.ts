a4d15fd06034605e0541ee624d916c75570b60fa
unknown <derianpaez01@hotmail.com>
2022-04-29 23:18:03 -0500

    add projects

--- data/projects.data.ts
+++ data/projects.data.ts
@@ -32,8 +32,8 @@ export const projects: ProjectType[] = [
   },
   {
     id: "003",
-    name: "Grupo Pronobis",
-    image: "/assets/projects/GrupoPronobis.png",
+    name: "Pacífica",
+    image: "/assets/projects/EdifPacifica.png",
     date: "20 ENERO, 2020",
     importance: true,
     worksDone: [
@@ -43,8 +43,8 @@ export const projects: ProjectType[] = [
   },
   {
     id: "004",
-    name: "La Joya",
-    image: "/assets/projects/LaJoya.png",
+    name: "Urbanización Casa Laguna",
+    image: "/assets/projects/CasaLaguna.png",
     date: "20 ENERO, 2020",
     importance: true,
     worksDone: [
@@ -54,8 +54,8 @@ export const projects: ProjectType[] = [
   },
   {
     id: "005",
-    name: "La Joya",
-    image: "/assets/projects/LaJoya.png",
+    name: "Urbanización Villas del Rey",
+    image: "/assets/projects/VillasDelRey.png",
     date: "20 ENERO, 2020",
     importance: false,
     worksDone: [
@@ -65,8 +65,8 @@ export const projects: ProjectType[] = [
   },
   {
     id: "006",
-    name: "La Joya",
-    image: "/assets/projects/LaJoya.png",
+    name: "Urbanización Napoli",
+    image: "/assets/projects/Napoli.png",
     date: "20 ENERO, 2020",
     importance: false,
     worksDone: [
@@ -78,6 +78,193 @@ export const projects: ProjectType[] = [
     id: "007",
     name: "Grupo Pronobis",
     image: "/assets/projects/GrupoPronobis.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "008",
+    name: "Urbanización Aires del Batan",
+    image: "/assets/projects/AiresDelBatan.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "009",
+    name: "Colegio Alemán Humboldt",
+    image: "/assets/projects/ColegioHumboldt.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0010",
+    name: "Urbanización Aires del Batan",
+    image: "/assets/projects/AiresDelBatan.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0011",
+    name: "Constructora Covigon",
+    image: "/assets/projects/Covigon.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0012",
+    name: "Urbanización Málaga II",
+    image: "/assets/projects/Malaga2.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0013",
+    name: "Urbanización Villa Club",
+    image: "/assets/projects/VillaClub.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0014",
+    name: "Urbanización Central Park",
+    image: "/assets/projects/CentralPark.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0015",
+    name: "Urbanización Bosques de Castilla",
+    image: "/assets/projects/BosquesDeCastilla.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0016",
+    name: "Urbanización Sambo City",
+    image: "/assets/projects/SamboCity.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0017",
+    name: "Urbanización Arrecife",
+    image: "/assets/projects/Arrecife.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0018",
+    name: "Urbanización Jardines de Alcalá",
+    image: "/assets/projects/JardinesDeAlcala.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0019",
+    name: "Urbanización Britania I",
+    image: "/assets/projects/Britania1.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0020",
+    name: "Urbanización Santa María",
+    image: "/assets/projects/SantaMaria.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0021",
+    name: "Urbanización Entre Lagos",
+    image: "/assets/projects/EntreLagos.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0022",
+    name: "Urbanización Rinconada Del Lago",
+    image: "/assets/projects/RinconadaDelLago.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0023",
+    name: "Urbanización Laguna Dorada",
+    image: "/assets/projects/LagunaDorada.png",
+    date: "20 ENERO, 2020",
+    importance: false,
+    worksDone: [
+      "Areas verdes",
+      "Sistema de riego"
+    ]
+  },
+  {
+    id: "0024",
+    name: "Urbanización Terra Sol",
+    image: "/assets/projects/TerraSol.png",
     date: "20 ENERO, 2020",
     importance: false,
     worksDone: [

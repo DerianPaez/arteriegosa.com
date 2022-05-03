59eef3f1ef8a8c23acc802c7b2c8d41795329953
James Mendoza <mendozajames02@hotmail.com>
2022-05-03 10:23:06 -0500

    Change number

--- data/contact.data.ts
+++ data/contact.data.ts
@@ -1,7 +1,7 @@
 export const contactData = {
   title: 'Comunicate Con Nosotros',
-  // cellphone: '593984169731'
-  cellphone: '593962852852',
+  cellphone: '593984169731'
+  // cellphone: '593962852852',
   serviceList: [
     { id: "001", label: "Sistema de riego", value:"Sistema de riego" },
     { id: "002", label: "Areas verdes", value:"Areas verdes" },

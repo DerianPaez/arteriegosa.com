4f8b60a1d6d0cdc0e97bb4b277ac9bc60f367407
James Mendoza <mendozajames02@hotmail.com>
2022-05-03 10:23:47 -0500

    Fix error in contact.data

--- data/contact.data.ts
+++ data/contact.data.ts
@@ -1,7 +1,6 @@
 export const contactData = {
   title: 'Comunicate Con Nosotros',
-  cellphone: '593984169731'
-  // cellphone: '593962852852',
+  cellphone: '593984169731',
   serviceList: [
     { id: "001", label: "Sistema de riego", value:"Sistema de riego" },
     { id: "002", label: "Areas verdes", value:"Areas verdes" },
